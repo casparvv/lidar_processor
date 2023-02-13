dcb75446f1bb81126a213e25530ce9a543f3d50c
Caspar van Venrooij <casparvv52@gmail.com>
2023-02-13 14:00:29 +0100

    Clean up

--- src/lidar_processor.cpp
+++ src/lidar_processor.cpp
@@ -1,4 +1,8 @@
-// LiDAR point cloud processing node
+// LiDAR point cloud processing node.
+
+// To do: converse data
+// Current DSI fabrics input (n*2,) --processing in planner--> (n, 3)
+// Give pcl output to adapt_runtime_arguments as third argument
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
@@ -11,19 +15,25 @@ ros::Publisher pub;
 void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
 {
   // // Callback with downsampled VoxelGrid filter
+  // Container for original & filtered data
   pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2; 
   pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
   pcl::PCLPointCloud2 cloud_filtered;
 
+  // Convert to PCL data type
   pcl_conversions::toPCL(*input, *cloud);
 
+  // Perform the actual filtering
   pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
   sor.setInputCloud (cloudPtr);
   sor.setLeafSize (0.1, 0.1, 0.1);
   sor.filter (cloud_filtered);
 
+  // Convert to ROS data type
   sensor_msgs::PointCloud2 output;
   pcl_conversions::moveFromPCL(cloud_filtered, output);
+
+  // Publish the data
   pub.publish (output);
 
   // // Simple callback: publish raw PCL2 (for testing)
@@ -38,7 +48,7 @@ int main (int argc, char** argv)
   ros::NodeHandle nh;
 
   ros::Subscriber sub = nh.subscribe ("/lidar/pointcloud", 1, cloud_cb);
-  pub = nh.advertise<sensor_msgs::PointCloud2> ("point_cloud_filtered", 1);
+  pub = nh.advertise<sensor_msgs::PointCloud2> ("/point_cloud_filtered", 1);
 
   ros::spin ();
 }
